6532fffd86a7bd731a1e1ba30dbc1ec44360316c
Kazue Igarashi <igarashi@digitalcube.jp>
2017-06-01 20:35:37 +0900

    デプロイ時のgit操作でファイルのパーミッションを無視する

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git config core.filemode false
 git fetch
 git checkout -b release origin/release
-git merge origin/master
+git add --all
+git commit -m "Automatic Build by Travis CI $TRAVIS_COMMIT"
 git push git@github.com:jaws-ug/jaws-ug.jp.git release
