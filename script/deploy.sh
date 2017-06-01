88c4750be71ffe261e19dcdb1b1a562e5fcad4b5
Kazue Igarashi <igarashi@digitalcube.jp>
2017-06-01 18:09:43 +0900

    デプロイ部分の変更

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,6 @@
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch
 git checkout -b release origin/release
-git merge origin/master
+git add --all
+git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER)"
 git push git@github.com:jaws-ug/jaws-ug.jp.git release
