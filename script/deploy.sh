001be0a103531543c2216d1dde3aae7224cc42cd
Kazue Igarashi <igarashi@digitalcube.jp>
2017-06-01 16:35:58 +0900

    travis デプロイ

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,5 @@
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch
 git checkout -b release origin/release
-git add --all
-git commit -m "Automatic Build by Travis CI $TRAVIS_COMMIT"
+git merge origin/master
 git push git@github.com:jaws-ug/jaws-ug.jp.git release
